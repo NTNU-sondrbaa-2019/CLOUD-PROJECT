6a31da2689db73f1d0e6126d03ab99fe76a71aa5
sindreoestrem <sindrost@stud.ntnu.no>
2019-11-20 19:28:22 +0100

    Cron changes in main.go

--- cmd/gr8elo/main.go
+++ cmd/gr8elo/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/NTNU-sondrbaa-2019/CLOUD-O1/pkg/CO1Cache"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/pkg/rating"
 	"github.com/robfig/cron/v3"
+	"net/http"
 )
 
 func main() {
@@ -28,9 +29,12 @@ func main() {
 
 	// Uncomment to run the lichess stuff.
 
+	// Go service must be running for the cron job to take place
 	c := cron.New()	
 	teamIdKey := "storbukk-sjakklubb"
-	_, err := c.AddFunc("0 2 * * *", func() {
+	//_, err := c.AddFunc("0 2 * * *", func() {
+	// For testing purposes run every 10 minutes
+	_, err := c.AddFunc("*/10 * * * *", func() {
 		rating.GetTeamElo(teamIdKey)
 	})
 
@@ -39,6 +43,7 @@ func main() {
 	}
 
 	c.Start()
+	_ = http.ListenAndServe(":8080", nil)
 }
 
 
