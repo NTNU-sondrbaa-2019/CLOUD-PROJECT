7aa939b4f20804d89141c61d080189acd42e5f4a
Infroz <infroz98@gmail.com>
2019-11-21 00:01:04 +0100

    Removed Clutter

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"regexp"
@@ -14,17 +13,4 @@ func main() {
 
 	log.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// m is the string after api/v1/ example "user/results/"
-		m := validPath.FindStringSubmatch(r.URL.Path) // Checks if it matches criteria
-		if m == nil {
-			fmt.Println(m[3])
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2]) // Returns a "handler" with w(res) r(req) m(string)
-	}
 }
