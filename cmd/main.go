93ae46c8a015f961c5244fa476de69d307d6b893
Infroz <infroz98@gmail.com>
2019-11-20 10:40:01 +0100

    works, but is messy

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,75 @@
 package main
 
 import (
-	"fmt"
-	"github.com/NTNU-sondrbaa-2019/CLOUD-O1/pkg/CO1Cache"
+	"errors"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
 )
 
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 func main() {
+	http.HandleFunc("/api/", makeHandler(viewHandler))
 
-	type Test struct {
-		Name string `json:"name"`
-		Author string `json:"author"`
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
 	}
+	return &Page{Title: title, Body: body}, nil
+}
 
-	test := Test {
-		"This is a test JSON",
-		"Sondre Benjamin Aasen",
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
 	}
+	renderTemplate(w, "view", p)
+}
 
-	CO1Cache.Initialize()
-	CO1Cache.WriteJSON("test", test)
-	
-	
-	http.HandleFunc("/api/", makeHandler(testHandler))
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil // The title is the second subexpression.
 }
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        m := validPath.FindStringSubmatch(r.URL.Path)
-        if m == nil {
-            http.NotFound(w, r)
-            return
-        }
-        fn(w, r, m[2])
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
 }
