42631dd87b851adc9290a2f81ef30517bfe02259
Infroz <infroz98@gmail.com>
2019-11-21 00:07:43 +0100

    Commented out not existed handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"CLOUD-PROJECT/internal"
 	"log"
 	"net/http"
 )
@@ -9,7 +8,7 @@ import (
 func main() {
 	// 2 Options, either all handle requests are made here with new handlers
 	// Or each endpoint can be handled via switch in a "mainHandler"
-	http.HandleFunc("/api/v1/", internal.MakeHandler(handlerTest))
+	//http.HandleFunc("/api/v1/", internal.MakeHandler(someHandler))
 
 	log.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
