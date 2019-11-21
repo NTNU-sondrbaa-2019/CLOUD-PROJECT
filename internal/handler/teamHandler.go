2a684484f07202f96866779c3e884941d35f6417
Infroz <infroz98@gmail.com>
2019-11-21 17:54:59 +0100

    created fake response on /team/

--- internal/handler/teamHandler.go
+++ internal/handler/teamHandler.go
@@ -1,18 +1,20 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 type Team struct {
 	Name 		string
 	CreatedAt 	string
-	members 	[]User
+	Members 	[]User
 }
 
 type User struct {
-
+	Username string
 }
 
 func teamHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -21,7 +23,22 @@ func teamHandler(w http.ResponseWriter, r *http.Request, title string) {
 
 	switch r.Method {
 	case "GET":
-		
+		localTeam := Team{
+			Name: "Name",
+			CreatedAt: "Date",
+			Members: nil,
+		}
+
+		// Encode new structure to JSON format
+		enc, err := json.Marshal(localTeam)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Gives JSON response for requests
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(enc)
 	default:
 		// Not supported...
 	}
