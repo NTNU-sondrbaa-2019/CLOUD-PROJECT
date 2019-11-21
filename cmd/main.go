dfb6488f749618df6868fe01859c2234c097aad0
Infroz <infroz98@gmail.com>
2019-11-21 11:29:35 +0100

    Handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,25 @@
 package main
 
 import (
-	"log"
-	"net/http"	"fmt"
+	"fmt"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-O1/pkg/CO1Cache"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/rating"
+	"log"
+	"net/http"
 )
 
 import "github.com/robfig/cron/v3"
 
 func main() {
-	// 2 Options, either all handle requests are made here with new handlers
-	// Or each endpoint can be handled via switch in a "mainHandler"
-	//http.HandleFunc("/api/v1/", internal.MakeHandler(someHandler))
-	log.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
 	type Test struct {
-		Name string `json:"name"`
+		Name   string `json:"name"`
 		Author string `json:"author"`
 	}
 
-	test := Test {
+	test := Test{
 		"This is a test JSON",
 		"Sondre Benjamin Aasen",
 	}
-
-
 
 	CO1Cache.Initialize()
 	CO1Cache.WriteJSON("test", test)
@@ -46,7 +39,13 @@ func main() {
 	}
 
 	c.Start()
+
+	// 2 Options, either all handle requests are made here with new handlers
+	// Or each endpoint can be handled via switch in a "mainHandler"
+	//http.HandleFunc("/api/v1/", internal.MakeHandler(someHandler))
+
+	http.HandleFunc("", makeHandler())
+
+	log.Println("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-
-
---
+++ internal/handler/UserHandler.go
@@ -0,0 +1,6 @@
+package handler
+
+
+func UserHandler(w http.ResponseWriter, r *http.Request, title string) {
+
+}
