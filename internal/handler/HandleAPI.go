fc6edb67693a7ea12d34275e7d51922c201cfb72
adrianchambeeng <adrianec@stud.ntnu.no>
2019-11-21 17:23:46 +0100

    Added handlers

--- internal/handler/HandleAPI.go
+++ internal/handler/HandleAPI.go
@@ -9,11 +9,11 @@ import (
 // HandleAPI - All requests to API endpoints will be handled here, follow testHandler for example
 func HandleAPI(w http.ResponseWriter, r *http.Request, title string) {
     //http.HandleFunc("/api/v1/test/", MakeHandler(testHandler)) // Should be removed in final version
-    http.HandleFunc("api/v1/login", MakeHandler(gauth.LoginHandler))
-    http.HandleFunc("api/v1/loggedin", MakeHandler(gauth.LoggedInHandler))
-    http.HandleFunc("api/v1/logout", MakeHandler(gauth.LogoutHandler))
+    http.HandleFunc("/api/v1/login/", MakeHandler(gauth.LoginHandler))
+    http.HandleFunc("/api/v1/loggedin/", MakeHandler(gauth.LoggedInHandler))
+    http.HandleFunc("/api/v1/logout/", MakeHandler(gauth.LogoutHandler))
 
-    http.HandleFunc("api/v1/authcallback", MakeHandler(gauth.OauthCallBackHandler))
+    http.HandleFunc("/api/v1/authcallback/", MakeHandler(gauth.OauthCallBackHandler))
 
     // Here we could make a html documentation file for our API for now it returns not found.
     http.NotFound(w, r)
