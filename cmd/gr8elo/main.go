08e2ad7112c6f16cfb413af9d6c2604937fcf7f8
Sondre Benjamin Aasen <eleljrk@gmail.com>
2019-11-21 15:19:40 +0100

    Began implementing database package

--- cmd/gr8elo/main.go
+++ cmd/gr8elo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-O1/pkg/CO1Cache"
-
+	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/database"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/handler"
 
 	"log"
@@ -16,25 +16,60 @@ import (
 )
 
 func main() {
-	type Test struct {
-		Name   string `json:"name"`
-		Author string `json:"author"`
-	}
 
-	test := Test{
-		"This is a test JSON",
-		"Sondre Benjamin Aasen",
-	}
-
+	// initialize cache
+	log.Println("Initializing cache...")
 	CO1Cache.Initialize()
-	CO1Cache.WriteJSON("test", test)
 
-	fmt.Println("Hello World!")
+	// TODO Sondre: Move to database package
+
+	// write database config to cache
+	log.Println("Initializing database...")
+	if !CO1Cache.Verify("db-config") {
+		log.Println("Writing default database configuration...")
+		CO1Cache.WriteJSON("db-config", database.DEFAULT_CONNECTION)
+		log.Fatalln("To test the AWS database from localhost, please insert password into the generated ./cache/db-config.json file!")
+	} else {
+
+		var db database.DatabaseConnection
+		err := json.Unmarshal(CO1Cache.Read("db-config"), &db)
+
+		db_host := os.Getenv("DB_HOST")
+
+		if db_host != "" {
+			db.Host = db_host
+		}
+
+		db_port := os.Getenv("DB_PORT")
+
+		if db_port != "" {
+			db.Port = db_port
+		}
+
+		db_username := os.Getenv("DB_USERNAME")
+
+		if db_username != "" {
+			db.Username = db_username
+		}
+
+		db_password := os.Getenv("DB_PASSWORD")
+
+		if db_password != "" {
+			db.Password = db_password
+		}
+
+		// TODO Sondre: Error handling of invalid HOST, PORT, USERNAME, PASSWORD
+
+		if err != nil {
+			log.Fatalln("Couldn't read database connection data...")
+		}
+
+		// TODO Sondre: connect to database
+	}
 
 	// Uncomment to run the lichess stuff.
-
 	// Go service must be running for the cron job to take place
-	c := cron.New()	
+	c := cron.New()
 	teamIdKey := "storbukk-sjakklubb"
 	//_, err := c.AddFunc("0 2 * * *", func() {
 	// For testing purposes run every 10 minutes
@@ -62,5 +97,5 @@ func main() {
 	}
 
 	log.Println("Listening on port " + port)
-	log.Fatal(http.ListenAndServe(":" + port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
