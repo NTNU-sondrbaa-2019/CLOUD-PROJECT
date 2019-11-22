876846f94bf9db5ee8dc090a5d0cdd9d5226edff
Sondre Benjamin Aasen <eleljrk@gmail.com>
2019-11-22 01:26:14 +0100

    Added more environment variables in main.go, fixed some minor issues inside main.go, began implementing database (again)

--- cmd/gr8elo/main.go
+++ cmd/gr8elo/main.go
@@ -1,45 +1,53 @@
 package main
 
 import (
-	"fmt"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-O1/pkg/CO1Cache"
+	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/database"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/handler"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/pkg/rating"
+	"github.com/robfig/cron/v3"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-import "github.com/robfig/cron/v3"
-
 func main() {
-	type Test struct {
-		Name   string `json:"name"`
-		Author string `json:"author"`
-	}
 
-	test := Test{
-		"This is a test JSON",
-		"Sondre Benjamin Aasen",
-	}
-
+	// initialize cache
+	log.Println("Initializing cache...")
 	CO1Cache.Initialize()
-	CO1Cache.WriteJSON("test", test)
 
-	fmt.Println("Hello World!")
+	// write database config to cache
+	log.Println("Initializing database...")
+	database.Connect()
 
-	// Uncomment to run the lichess stuff.
-
+	// Setting up cronjobs
+	log.Println("Initializing automatic ELO fetching...")
 	c := cron.New()
-	teamIdKey := "storbukk-sjakklubb"
+
+	// Get lichess.org teams to automatically fetch data about from environment
+	lichess_teamids := os.Getenv("LICHESS-TEAMS")
+
+	// Get default if nothing in environment
+	if lichess_teamids == "" {
+		lichess_teamids = rating.LICHESS_DEFAULT_TEAMS
+	}
+
+	// Add cronjob
 	_, err := c.AddFunc("0 2 * * *", func() {
-		rating.GetTeamElo(teamIdKey)
+		for _, lichess_teamid := range strings.Split(lichess_teamids, ",") {
+			rating.GetTeamElo(lichess_teamid)
+		}
 	})
 
 	if err != nil {
-		panic(err)
+
+		log.Println(err)
+
 	}
 
+	// Start cronjobs
 	c.Start()
 
 	// 2 Options, either all handle requests are made here with new handlers
@@ -56,5 +64,5 @@ func main() {
 	}
 
 	log.Println("Listening on port " + port)
-	log.Fatal(http.ListenAndServe(":" + port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
--- internal/pkg/rating/constants.go
+++ internal/pkg/rating/constants.go
@@ -1,4 +1,5 @@
 package rating
 
-// LichessRateLimitSeconds for sleep when we hit the rate limit of lichess.org
-const LichessRateLimitSeconds = 61;
+// LICHESS_RATE_LIMIT for sleep when we hit the rate limit of lichess.org
+const LICHESS_RATE_LIMIT = 61;
+const LICHESS_DEFAULT_TEAMS = "storbukk-sjakklubb"
