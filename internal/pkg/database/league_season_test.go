4a316a9eae7599be0954e1f76a5e7cf742fdb25c
Sondre Benjamin Aasen <sondreaasen@hotmail.com>
2019-11-23 21:47:15 +0100

    Added empty test files for everything in database

---
+++ internal/pkg/database/league_season_test.go
@@ -0,0 +1,25 @@
+package database_test
+
+import (
+	"github.com/NTNU-sondrbaa-2019/CLOUD-O1/pkg/CO1Cache"
+	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/pkg/database"
+	"testing"
+)
+
+func TestLeagueSeason(t *testing.T) {
+
+	CO1Cache.Initialize()
+	database.Connect()
+
+	_, err := database.GetConnection().Exec("START TRANSACTION")
+
+	if err != nil {
+		_, _ = database.GetConnection().Exec("ROLLBACK")
+		t.Fatal(err)
+	}
+
+	// TODO tests
+
+	_, _ = database.GetConnection().Exec("ROLLBACK")
+
+}
