4aa8433040da4f1c58f5e032b8a8a19b5b676431
Infroz <infroz98@gmail.com>
2019-11-23 18:23:49 +0100

    team/users implemented

--- internal/pkg/api/teamHandler.go
+++ internal/pkg/api/teamHandler.go
@@ -17,8 +17,7 @@ var grouping *[]database.GROUP_USER
 
 func TeamHandler(w http.ResponseWriter, r *http.Request) HTTPErrors.Error {
 	urlPart := strings.Split(r.URL.Path, "/")
-	fmt.Println(len(urlPart))
-	fmt.Println(urlPart[4] != "")
+
 	switch r.Method {
 	case "GET":
 		if (urlPart[4] != "") {
@@ -61,8 +60,43 @@ func TeamHandler(w http.ResponseWriter, r *http.Request) HTTPErrors.Error {
 }
 
 func TeamUsersHandler(w http.ResponseWriter, r *http.Request) HTTPErrors.Error {
+	// Struct to store data gathered
+	type returnType struct {
+		TeamName string
+		Users []database.USER
+	}
+
+	var someValues []returnType
 	fmt.Println("Finding Users in team...")
+	// Since team is array this will return multiple teams
+	for i, s := range *team {
+		fmt.Println("Printing team...")
+		fmt.Println(i,s)
+		var tmpValues returnType
+		grouping, _ = database.SelectGroupUserByGroupID(s.ID)
+		tmpValues.TeamName = s.Name
+
+		// Loop through usersgroups
+		for n, ug := range *grouping {
+			fmt.Println("Printing users")
+			fmt.Println(n, ug)
+			user, _ = database.SelectUserByID(ug.UserID)
+
+			tmpValues.Users = append(tmpValues.Users, *user)
+		}
+		someValues = append(someValues, tmpValues)
+	}
 	
+	// Encode new structure to JSON format
+	enc, err := json.Marshal(someValues)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Gives JSON response for requests
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(enc)
 
 	return HTTPErrors.NewError("", 0)
 }
