3bd0a55fdaff880907498a2c1a694d69344456ab
Infroz <infroz98@gmail.com>
2019-11-26 19:12:38 +0100

    Fixed 404 error

--- internal/pkg/index/indexHandler.go
+++ internal/pkg/index/indexHandler.go
@@ -1,6 +1,7 @@
 package index
 
 import (
+	"fmt"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/pkg/HTTPErrors"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/pkg/gauth"
 	"github.com/NTNU-sondrbaa-2019/CLOUD-PROJECT/internal/pkg/view"
@@ -16,30 +17,36 @@ type data struct {
 }
 
 func HandleIndex(w http.ResponseWriter, r *http.Request) HTTPErrors.Error {
-	logged := false // Doesnt check currently if actually logged in
-	currentTime := time.Now()
+	url := r.URL.Path
+	fmt.Println(url)
+	if url == "/" {
+		logged := false // Doesnt check currently if actually logged in
+		currentTime := time.Now()
 
-	// Gets the value od the sessionID cokkie from the users browser
-	sessionID := gauth.GetCookieValueByName(r.Cookies(), "sessionID")
-	// If the sessionID is not empty, we are logged in
-	if sessionID != "" {
-		logged = true
-	}
-
-	if !logged {
-		// Page to load if not logged in
-		page := &data{
-			Title:       "Login - gr8elo.com",
-			CurrentYear: strconv.Itoa(currentTime.Year()),
-			Username:    "Unknown Username",
+		// Gets the value od the sessionID cokkie from the users browser
+		sessionID := gauth.GetCookieValueByName(r.Cookies(), "sessionID")
+		// If the sessionID is not empty, we are logged in
+		if sessionID != "" {
+			logged = true
 		}
 
-		view.Render(w, "login", page)
-	} else {
-		// Page to load if logged in
-		page := &data{Title: "Homepage - gr8elo.com", CurrentYear: strconv.Itoa(currentTime.Year())}
+		if !logged {
+			// Page to load if not logged in
+			page := &data{
+				Title:       "Login - gr8elo.com",
+				CurrentYear: strconv.Itoa(currentTime.Year()),
+				Username:    "Unknown Username",
+			}
 
-		view.Render(w, "ucp", page)
+			view.Render(w, "login", page)
+		} else {
+			// Page to load if logged in
+			page := &data{Title: "Homepage - gr8elo.com", CurrentYear: strconv.Itoa(currentTime.Year())}
+
+			view.Render(w, "ucp", page)
+		}
+	} else {
+		view.ErrorPage(w, "Not found", http.StatusNotFound)
 	}
 	return HTTPErrors.NewError("", 0)
 }
